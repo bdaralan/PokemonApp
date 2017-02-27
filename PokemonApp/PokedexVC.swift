7f450226dab36de2e52cc5569f23ad6196a0556f
Dara <bd.daralan@yahoo.com>
2017-02-26 22:13:44 -0800

    made search for pokemon caseInsensitive

--- PokemonApp/PokedexVC.swift
+++ PokemonApp/PokedexVC.swift
@@ -66,7 +66,7 @@ class PokedexVC: UIViewController, UITableViewDataSource, UITableViewDelegate, U
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText != "" {
             inSearchMode = true
-            pokemon = allPokemon.filter({ $0.name.range(of: searchText) != nil })
+            pokemon = allPokemon.filter({ $0.name.range(of: searchText, options: .caseInsensitive) != nil })
         } else {
             inSearchMode = false
             pokemon = allPokemon
